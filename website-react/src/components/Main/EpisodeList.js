2d561bf4c3e6721771193e6f3cbfa5497b4b8032
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-01 22:08:58 +0100

    removed unnecessary css

--- website-react/src/components/Main/EpisodeList.js
+++ website-react/src/components/Main/EpisodeList.js
@@ -1,5 +1,10 @@
 import styled from 'styled-components'
 
 export default styled.ul`
-
+    li {
+        list-style: none;
+        padding: 20px 5px 20px 5px;
+        margin: 0px;
+        background-color: #fafafa;
+    }
 `
--- website-react/src/components/Main/Search.js
+++ website-react/src/components/Main/Search.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import {actions} from 'mirrorx'
 
-export default props => (
+export default ({searchTerm}) => (
   <form onSubmit={event => {
     event.preventDefault()
     actions.search.search()
@@ -9,7 +9,7 @@ export default props => (
     <input
       placeholder='Type something here!'
       className='search-input'
-      value={props.searchTerm}
+      value={searchTerm}
       onChange={event => actions.search.updateSearchTerm(event.target.value)}
     />
   </form>
