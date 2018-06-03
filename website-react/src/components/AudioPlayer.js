7b190fc1733fa8e060d1dea8b3b91db48b8952df
THEjoezack <me@joezack.com>
2018-06-02 23:25:39 -0400

    Adding some minor styling to differentiate elements

--- website-react/src/components/AudioPlayer.js
+++ website-react/src/components/AudioPlayer.js
@@ -2,10 +2,15 @@ import styled from 'styled-components';
 
 export default styled.audio`
   position: fixed;
-  bottom: 0;
+  bottom: 20px;
   width: 100%;
 
   &::-webkit-media-controls-panel {
     background: #4F8F88;
+    padding: 10px 0 10px 0;
+  }
+
+  &::-webkit-media-controls-play-button {
+    color: white;
   }
 `
