8267d3b2a06e43b4228e97c26bf3dfe47b12051a
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-05 14:59:12 +0100

    added scroll smoothing and "next in queue" text

--- website-react/src/components/Main/Playlist.js
+++ website-react/src/components/Main/Playlist.js
@@ -11,28 +11,33 @@ export default ({results, playlist, nowPlaying}) => (
   playlist.length === 0
     ? `No episodes added to your playlist yet.
     Go ahead and search for some episodes to add!`
-    : playlist.map(episode =>
-      <Episode
-        onClick={() => actions.player.play(episode)}
-        key={episode.id}
-        playing={episode.audioUrl === nowPlaying.audioUrl}
-      >
-        <EpisodeTitle>{episode.episodeTitle}</EpisodeTitle>
-        <PodcastTitle>{episode.podcastTitle}</PodcastTitle>
+    : <div id="queue">
+    Next in queue:
+      {
+        playlist.map(episode =>
+          <Episode
+            onClick={() => actions.player.play(episode)}
+            key={episode.id}
+            playing={episode.audioUrl === nowPlaying.audioUrl}
+          >
+            <EpisodeTitle>{episode.episodeTitle}</EpisodeTitle>
+            <PodcastTitle>{episode.podcastTitle}</PodcastTitle>
 
-        <PlayNextButton
-          onClick={event => {
-            event.stopPropagation()
-            actions.player.playNext(episode)
-          }}
-        />
+            <PlayNextButton
+              onClick={event => {
+                event.stopPropagation()
+                actions.player.playNext(episode)
+              }}
+            />
 
-        <RemoveFromPlaylistButton
-          onClick={event => {
-            event.stopPropagation()
-            actions.player.removeFromPlaylist(episode.id)
-          }}
-        />
-      </Episode>
-    )
+            <RemoveFromPlaylistButton
+              onClick={event => {
+                event.stopPropagation()
+                actions.player.removeFromPlaylist(episode.id)
+              }}
+            />
+          </Episode>
+        )
+      }
+    </div>
 )
--- website-react/src/components/NowPlaying.js
+++ website-react/src/components/NowPlaying.js
@@ -83,25 +83,3 @@ export default styled(NowPlaying)`
     }
   }
 `
-
-
-// @search.score:
-// 0.3551399
-
-// audioUrl:
-// "https://aphid.fireside.fm/d/1437767933/b44de5fa-47c1-4e94-bf9e-c72f8d1c8f5d/0dba1f52-f433-47b6-9a31-f1a1fa5b4380.mp3"
-
-// description:
-// "<p>Mike may have cracked the testing pitch, the harsh reason the Junior Developer is dying & a nice batch of audience questions and follow up.</p>↵↵<p><p>This week’s Coder Radio is just like mom’s cookin&#39;, but with a taste of Kotlin.</p><img src="http://feeds.feedburner.com/~r/coderradiomp3/~4/4wm8patYirM" height="1" width="1" alt=""/></p>"
-
-// episodeTitle:
-// "Lunch Break Coder | CR 297"
-
-// id:
-// "125523CE_C660_49C5_81A8_24A8F17172A7"
-
-// podcastTitle:
-// "Coder Radio"
-
-// published:
-// "2018-02-20T00:00:00.000Z"
