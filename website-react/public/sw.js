ad4f2e13aae062a350ad35c2bf1c7c43901e7c59
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-07 20:15:48 +0100

    bumped sw version

--- website-react/public/sw.js
+++ website-react/public/sw.js
@@ -1,8 +1,8 @@
 /* eslint-env serviceworker */
 /* global fetch */
 
-const dataCacheName = 'podcasts-data-v6'
-const cacheName = 'podcasts-v6'
+const dataCacheName = 'podcasts-data-v7'
+const cacheName = 'podcasts-v7'
 const filesToCache = [
   '/',
   '/index.html',
