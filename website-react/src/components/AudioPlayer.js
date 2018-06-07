59412d9ed01a486250e20ecefbf9cf40525ea1b2
NMarcora <nicolas.marcora@yahoo.co.uk>
2018-06-07 20:46:32 +0100

    added auto-hide keyboard on mobile when search is performed

--- website-react/src/components/AudioPlayer.js
+++ website-react/src/components/AudioPlayer.js
@@ -1,7 +1,10 @@
 import styled from 'styled-components'
 
 export default styled.audio`
-  width: calc(100% -4px);
+  width: calc(99%);
+  max-width: 796px;
+  position: fixed;
+  bottom: 2px;
 
   &::-webkit-media-controls-panel {
     background: #4F8F88;
