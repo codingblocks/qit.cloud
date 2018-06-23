560d226764850bd9f0fd288e77cdbc8b3421b0ac
Mikkel Madsen <m@madsn.net>
2018-06-23 15:10:41 +0200

    Attempt at fixing SSL proxy issue with redirecting urls

--- website-react/src/config.js
+++ website-react/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  sslProxyUrl: 'https://ssl-proxy-izufsoiwnv.now.sh/?url=',
+  sslProxyUrl: 'https://ssl-proxy-oefakuyidz.now.sh/?url=',
   baseUrl: 'https://podcasts.search.windows.net/indexes/podcasts/docs?api-version=2017-11-11&$count=true&search=',
   apiKey: 'C7AC76C4D8E4FE369B5608D13A98468F',
   playbackrates: [1, 1.5, 1.75, 2]
