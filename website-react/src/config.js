94725808e11370de5d522baabd875d8b9ac8b051
Mikkel Madsen <m@madsn.net>
2018-06-25 19:57:54 +0200

    Order search results by episode published date

--- website-react/src/config.js
+++ website-react/src/config.js
@@ -1,5 +1,5 @@
 export default {
-  baseUrl: 'https://podcasts.search.windows.net/indexes/podcasts/docs?api-version=2017-11-11&$count=true&$top=200&queryType=full&search={searchTerm}', // note, appending a ~1 to the search term will enable fuzzy searching...but I don't recommend it
+  baseUrl: 'https://podcasts.search.windows.net/indexes/podcasts/docs?api-version=2017-11-11&$count=true&$top=200&queryType=full&$orderby=published desc&search={searchTerm}', // note, appending a ~1 to the search term will enable fuzzy searching...but I don't recommend it
   apiKey: 'C7AC76C4D8E4FE369B5608D13A98468F',
   corsProxy: 'https://cors-anywhere.herokuapp.com/',
   playbackRates: [1, 1.5, 1.75, 2]
