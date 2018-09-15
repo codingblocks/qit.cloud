8ff818f22fa8934ef6ba9b8100a66032a2abb8cf
Dave Follett <davefollet@gmail.com>
2018-09-15 00:01:52 -0400

    adding <li> element around the default empty search text for a11y.

--- website-react/src/components/Main/Queue.js
+++ website-react/src/components/Main/Queue.js
@@ -16,48 +16,50 @@ import {
 } from './Episode/Styled'
 
 export const Queue = ({ queue, nowPlaying, className }) =>
-  <div className={className}>
-    {queue.length === 0 ? null : 'Next in queue:'}
-    {queue.length === 0 ? (
-      `Your queue is empty. Try a search like "pwa" to learn more about the technology behind this app!`
-    ) : (
-      <SortableList
-        useWindowAsScrollContainer
-        useDragHandle
-        onSortEnd={({ oldIndex, newIndex }) =>
-          actions.player.resortQueue({ oldIndex, newIndex })
-        }
-        items={queue.map(episode =>
-          <StyledEpisode
-            onClick={() => actions.player.play(episode)}
-            key={episode.id}
-            playing={episode.audioUrl === nowPlaying.audioUrl}
-            data-item-type='queue'
-          >
-            <StyledEpisodeTitle>{episode.episodeTitle}</StyledEpisodeTitle>
+  <li>
+    <div className={className}>
+      {queue.length === 0 ? null : 'Next in queue:'}
+      {queue.length === 0 ? (
+        `Your queue is empty. Try a search like "pwa" to learn more about the technology behind this app!`
+      ) : (
+        <SortableList
+          useWindowAsScrollContainer
+          useDragHandle
+          onSortEnd={({ oldIndex, newIndex }) =>
+            actions.player.resortQueue({ oldIndex, newIndex })
+          }
+          items={queue.map(episode =>
+            <StyledEpisode
+              onClick={() => actions.player.play(episode)}
+              key={episode.id}
+              playing={episode.audioUrl === nowPlaying.audioUrl}
+              data-item-type='queue'
+            >
+              <StyledEpisodeTitle>{episode.episodeTitle}</StyledEpisodeTitle>
 
-            <StyledEpisodeBody>
-              <StyledPodcastTitle>
-                {episode.podcastTitle}&nbsp;
-                <PodcastReleaseDate releaseDate={episode.published} />
-              </StyledPodcastTitle>
+              <StyledEpisodeBody>
+                <StyledPodcastTitle>
+                  {episode.podcastTitle}&nbsp;
+                  <PodcastReleaseDate releaseDate={episode.published} />
+                </StyledPodcastTitle>
 
-              <StyledControls>
-                <RemoveFromQueueButton
-                  lonely={queue.length === 1}
-                  onClick={event => {
-                    event.stopPropagation()
-                    actions.player.removeFromQueue(episode)
-                  }}
-                />
-                {queue.length > 1 && <DragNDropIndicator />}
-              </StyledControls>
-            </StyledEpisodeBody>
-          </StyledEpisode>
-        )}
-      />
-    )}
-  </div>
+                <StyledControls>
+                  <RemoveFromQueueButton
+                    lonely={queue.length === 1}
+                    onClick={event => {
+                      event.stopPropagation()
+                      actions.player.removeFromQueue(episode)
+                    }}
+                  />
+                  {queue.length > 1 && <DragNDropIndicator />}
+                </StyledControls>
+              </StyledEpisodeBody>
+            </StyledEpisode>
+          )}
+        />
+      )}
+    </div>
+  </li>
 
 export default styled(Queue)`
   ${props => props.blur && 'filter: blur(5px);'};
