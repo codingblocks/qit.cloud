c8d308da7c0de0b3c9277e7b7f34686420c3ce19
Dave Follett <davefollet@gmail.com>
2018-09-15 21:34:09 -0400

    Changed the header green to provide more contrast with the white text. Also matched the link color to the new header color because it was also had a contrast issue.

--- website-react/src/components/Header/index.js
+++ website-react/src/components/Header/index.js
@@ -5,7 +5,7 @@ export default styled.header`
   max-width: 800px;
   height: 56px;
   color: #FFF;
-  background: #359189;
+  background: #298478;
   position: fixed;
   font-size: 20px;
   box-shadow: 0 4px 5px 0 rgba(0, 0, 0, 0.14), 0 2px 9px 1px rgba(0, 0, 0, 0.12), 0 4px 2px -2px rgba(0, 0, 0, 0.2);
