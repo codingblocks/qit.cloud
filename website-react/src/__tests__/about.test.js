6a41301fa06f1f99eae4626a69bc75b7c01d5408
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-01 14:37:09 -0700

    Trying for jest-dom again

--- website-react/src/__tests__/about.test.js
+++ website-react/src/__tests__/about.test.js
@@ -1,15 +1,22 @@
-import 'jest-dom/extend-expect'
+import {
+  toBeInTheDocument
+} from 'jest-dom'
+
+expect.extend({
+  toBeInTheDocument
+})
 
 it('this will be the only test that runs', () => {
-    expect(true).toEqual(true);
-  });
-  
- 
+  expect(true).toEqual(true);
+});
+
+
 
 window.it('should have an inner content area', () => {
-    const ancestor = document.querySelector('[className="innerContent"]') 
-    const inner = document.querySelector('[className="finePrint"]')
-    const notHere = document.querySelector('[id="doesNotExist"]',)
-    expect(inner).toBeInTheDocument()
-    expect(notHere).not.toBeInTheDocument()
+  const ancestor = document.querySelector('[className="innerContent"]')
+  const inner = document.querySelector('[className="finePrint"]')
+  const notHere = document.querySelector('[id="doesNotExist"]', )
+  expect(ancestor).toBeInTheDocument()
+  expect(inner).toBeInTheDocument()
+  expect(notHere).not.toBeInTheDocument()
 });
