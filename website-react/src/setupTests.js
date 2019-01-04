48f208ee1300fdc615fa1b6b7375afbbd53c8bec
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-04 14:02:14 -0700

    Moving away from unit tests, and insuring JSON is accurate

--- website-react/src/setupTests.js
+++ website-react/src/setupTests.js
@@ -1,5 +1,7 @@
 // This is a magical file that create-react-app and jest use for boilerplate config
 // https://github.com/facebook/create-react-app/blob/master/packages/react-scripts/template/README.md#initializing-test-environment
+import 'jest-dom/extend-expect'
+
 import { configure } from 'enzyme'
 import Adapter from 'enzyme-adapter-react-16'
 
