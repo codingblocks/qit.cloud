25b25fc6a05e3adc9ba4fadb3427650a42c64bb1
THEjoezack <me@joezack.com>
2019-01-06 17:19:16 -0500

    Adding min date

--- podcast-feed-loader/app/azureSearchUpdater.js
+++ podcast-feed-loader/app/azureSearchUpdater.js
@@ -2,16 +2,24 @@ const settings = require('./azureSettings').settings
 const client = require('./azureClient').client
 
 exports.callback = function (feedResults, context = console) {
-  if (!feedResults.updateFeed.length) {
-    console.log(`Warning: skipping ${feedResults.updateFeed} because no updates`)
+  context.log(`Min date: ${settings.minDate}`)
+  const latestResults = settings.minDate
+    ? feedResults.updateFeed.filter(e => {
+      return new Date(e.published) > new Date(settings.minDate)
+    })
+    : feedResults.updateFeed
+  if (!latestResults.length) {
+    context.log(`Warning: skipping ${feedResults.feedUrl} because no updates`)
   }
-  client.addDocuments(settings.index, feedResults.updateFeed, function (err, results) {
+  client.addDocuments(settings.index, latestResults, function (err, results) {
     // optional error, or confirmation of each document being added
     if (err) {
-      console.error(`Error loading ${feedResults.feedUrl}`)
-      console.error(err)
+      context.error(`Error loading ${feedResults.feedUrl}`)
+      context.error(err)
     } else {
-      context.log(`${feedResults.updateFeed.length} documents loaded for ${feedResults.feedUrl}`)
+      context.log(
+        `${latestResults.length} documents loaded for ${feedResults.feedUrl}`
+      )
     }
   })
 }
