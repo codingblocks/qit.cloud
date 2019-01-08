30c4846e7178595d7f164d6a2456f76d912d4a80
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-08 11:59:49 -0700

    Basic tests moved, jest-dom and service-worker-mock added.

---
+++ website/src/__tests__/sw.test.js
@@ -0,0 +1,21 @@
+const makeServiceWorkerEnv = require('service-worker-mock')
+const makeFetchMock = require('service-worker-mock/fetch')
+
+describe('Service worker', () => {
+  beforeEach(() => {
+    Object.assign(
+      global,
+      makeServiceWorkerEnv(),
+      makeFetchMock()
+      // If you're using sinon ur similar you'd probably use below instead of makeFetchMock
+      // fetch: sinon.stub().returns(Promise.resolve())
+    )
+    jest.resetModules()
+  })
+  it('should add listeners', () => {
+    require('../sw.js')
+    expect(self.listeners['install']).toBeDefined()
+    expect(self.listeners['activate']).toBeDefined()
+    expect(self.listeners['fetch']).toBeDefined()
+  })
+})
