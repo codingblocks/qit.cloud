5fcfd53b6ac92abe478c58bd02f95f9e8d05df45
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-08 14:22:30 -0700

    Passing test for render added

--- website/src/__tests__/App.test.js
+++ website/src/__tests__/App.test.js
@@ -1,13 +1,13 @@
-/* import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import Index from '../components/Main/index'
 
 it('renders without crashing', () => {
   const div = document.createElement('div')
-  ReactDOM.render(<App />, div)
+  ReactDOM.render(<Index />, div)
   ReactDOM.unmountComponentAtNode(div)
 })
-*/
+
 // replacement test while real tests are not written
 
 window.it('should run smoke test', () => console.log('write real tests'))
