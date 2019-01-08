bbd826207e9afe4d8a831fefc0b966cef3ae1371
Arlene Andrews <kara_arlene@yahoo.com>
2019-01-08 15:55:44 -0700

    Delete app.test.js from src folder, check for messages showing.

--- website/src/__tests__/App.test.js
+++ website/src/__tests__/App.test.js
@@ -2,12 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import Index from '../components/Main/index'
 
+// replacement test while real tests are not written
+
+window.it('should run smoke test', () => console.log('write real tests'))
+
 it('renders without crashing', () => {
   const div = document.createElement('div')
   ReactDOM.render(<Index />, div)
   ReactDOM.unmountComponentAtNode(div)
+  })
+
+describe('Opening index', () =>{
+  it('Is an queue empty message', () => {
+    const div = document.createElement('div')
+  ReactDOM.render(<Index />, div)
+    expect.stringContaining('Your queue is empty. Try a search like "pwa" to learn more about the technology behind this app!')
+    ReactDOM.unmountComponentAtNode(div)
+  })
+  it('Is a search box placeholder', () =>{
+    const div = document.createElement('div')
+  ReactDOM.render(<Index />, div)
+    expect.stringContaining('Search for a great podcast here!')
+    ReactDOM.unmountComponentAtNode(div)
+  })
 })
-
-// replacement test while real tests are not written
-
-window.it('should run smoke test', () => console.log('write real tests'))
