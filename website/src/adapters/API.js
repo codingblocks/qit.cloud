f0a309bc860fe66a1df828016a0310b27c1dc945
MinimumViablePerson <nicolas.marcora@yahoo.co.uk>
2019-01-29 23:02:50 +0000

    reduce API method code duplication

--- website/src/adapters/API.js
+++ website/src/adapters/API.js
@@ -4,102 +4,70 @@
 import config from '../config'
 
 class API {
-  static signin (username, password) {
-    return fetch(API.signinUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username,
-        password
-      })
-    })
-      .then(resp => resp.json())
+
+  static init () {
+    this.baseUrl = config.baseApiUrl
+    this.userUrl = this.baseUrl + '/user'
+    this.signinUrl = this.baseUrl + '/signin'
+    this.signupUrl = this.baseUrl + '/signup'
+    this.queueEpisodeUrl = this.baseUrl + '/queue_episode'
+    this.unqueueEpisodeUrl = this.baseUrl + '/unqueue_episode'
+  }
+
+  static get (url) {
+    return fetch(url, {
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token')
+      }
+    }).then(resp => resp.json())
       .catch(e => {
         console.log(`API error: ${e}`)
         window.errorReporting.notify({
           error: `API error: ${e}`
         })
       })
+  }
+
+  static post (url, data) {
+    return fetch(url, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: localStorage.getItem('token')
+      },
+      body: JSON.stringify(data)
+    }).then(resp => resp.json())
+      .catch(e => {
+        console.log(`API error: ${e}`)
+        window.errorReporting.notify({
+          error: `API error: ${e}`
+        })
+      })
+  }
+
+  static signin (username, password) {
+    return this.post(this.signinUrl, {username, password})
   }
 
   static signup (username, password, email) {
-    return fetch(API.signupUrl, {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        username,
-        password,
-        email
-      })
-    })
-      .then(resp => resp.json())
-      .catch(e => {
-        console.log(`API error: ${e}`)
-        window.errorReporting.notify({
-          error: `API error: ${e}`
-        })
-      })
+    return this.post(this.signupUrl, { username, password, email })
   }
 
-  static getUser (token = localStorage.getItem('token')) {
-    return fetch(API.userUrl, {
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token
-      }
-    })
-      .then(resp => resp.json())
-      .catch(e => {
-        console.log(`API error: ${e}`)
-        window.errorReporting.notify({
-          error: `API error: ${e}`
-        })
-      })
+  static getUser () {
+    return this.get(this.userUrl)
   }
 
-  static queueEpisode (episode, token = localStorage.getItem('token')) {
-    return fetch(API.queueEpisodeUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token
-      },
-      body: JSON.stringify({ episode })
-    })
-      .then(resp => resp.json())
-      .catch(e => {
-        console.log(`API error: ${e}`)
-        window.errorReporting.notify({
-          error: `API error: ${e}`
-        })
-      })
+  static queueEpisode (episode) {
+    return this.post(this.queueEpisodeUrl, { episode })
   }
 
-  static unqueueEpisode (id, token = localStorage.getItem('token')) {
-    return fetch(API.unqueueEpisodeUrl, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-        Authorization: token
-      },
-      body: JSON.stringify({ id })
-    })
-      .then(resp => resp.json())
-      .catch(e => {
-        console.log(`API error: ${e}`)
-        window.errorReporting.notify({
-          error: `API error: ${e}`
-        })
-      })
+  static unqueueEpisode (id) {
+    return this.post(this.unqueueEpisodeUrl, { id })
   }
 }
 
-API.baseUrl = config.baseApiUrl
-API.userUrl = API.baseUrl + '/user'
-API.signinUrl = API.baseUrl + '/signin'
-API.signupUrl = API.baseUrl + '/signup'
-API.queueEpisodeUrl = API.baseUrl + '/queue_episode'
-API.unqueueEpisodeUrl = API.baseUrl + '/unqueue_episode'
+API.init()
 
 // I use this for testing API calls in the console
 // window.API = API
