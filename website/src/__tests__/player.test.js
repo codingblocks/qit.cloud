486cb5839742779d1a49748d361ad7f6977ea1ab
Arlene Andrews <kara_arlene@yahoo.com>
2019-02-14 15:25:07 -0700

    Made adjustments - not woorking

--- website/src/__tests__/player.test.js
+++ website/src/__tests__/player.test.js
@@ -3,16 +3,20 @@ import { render } from 'mirrorx'
 import App from '../RoutedApp'
 import { shallow } from 'enzyme'
 import sinon from 'sinon'
+import {mockEpisodes} from '../setup'
 
 
-import AudioPlayer from '..AudioPlayer'
+import Queue from '../components/Main/Queue'
 
 it('Smoke test', () => {
   expect(true).toEqual(true);
 });
 
 describe('Will play audio file', () => {
-  it('Will find specific file', () => {
-    expect(true).toEqual(true);
+  it('should render all queue episodes', () => {
+    const wrapper = shallow(<Queue queue={mockEpisodes} />)
+    expect(wrapper).toContain(mockEpisodes[0].episodeTitle)
+    expect(wrapper).toContain(mockEpisodes[1].episodeTitle)
+    expect(wrapper).toContain(mockEpisodes[2].episodeTitle)
   })
-})
+});
