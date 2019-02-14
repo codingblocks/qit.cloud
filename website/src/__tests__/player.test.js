0fd20a6fe337a716ca6cb32142d2151381b01d1f
Arlene Andrews <kara_arlene@yahoo.com>
2019-02-14 11:00:40 -0700

    Audio files added, and a hopeful test started.

--- website/src/__tests__/player.test.js
+++ website/src/__tests__/player.test.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import { render } from 'mirrorx'
 import App from '../RoutedApp'
-import { shallow } from 'enzyme/build'
+import { shallow } from 'enzyme'
 import sinon from 'sinon'
 
 
-import '../models/AudioPlayerModel'
+import AudioPlayer from '..AudioPlayer'
 
 it('Smoke test', () => {
   expect(true).toEqual(true);
