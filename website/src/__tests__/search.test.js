67d8928b125e5d77d66b8066eb0e703870a88043
Arlene Andrews <kara_arlene@yahoo.com>
2019-04-26 14:25:11 -0600

    Added TODO for describe.each

--- website/src/__tests__/search.test.js
+++ website/src/__tests__/search.test.js
@@ -21,6 +21,7 @@ describe('Search Props', () => {
   })
 
 describe('Input checks', () => {
+  //TODO - rewite this with a describe.each https://jestjs.io/docs/en/api#describeeachtable-name-fn-timeout to make it easy to test.
   it('Should have placeholder text for blank inputs', () => {
     const wrapper = shallow(<Header />)
     const search = wrapper.find('[data-testid="input"]')
@@ -33,6 +34,20 @@ describe('Input checks', () => {
       'Your queue is empty. Try a search like "pwa" to learn more about the technology behind this app!'
     )
     expect.stringContaining('Search for a great podcast here!')
-    })   
+    })
+    
+    it('Should have placeholder text for % inputs', () => {
+      const wrapper = shallow(<Header />)
+      const search = wrapper.find('[data-testid="input"]')
+      const inputs = {blank: '', pct:'%'}
+      expect(search.length).toBe(0)
+  
+      search.value = inputs.pct
+      expect(search.value).toBe('%')
+      expect.stringContaining(
+        'Your queue is empty. Try a search like "pwa" to learn more about the technology behind this app!'
+      )
+      expect.stringContaining('Search for a great podcast here!')
+      })   
   })
 })
